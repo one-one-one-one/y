d50c4e2044771601f0068b1fa01522cda93a7f01
yusuf-ardiansyah <yusuf-ardiansyah@users.noreply.github.com>
2017-05-30 05:10:36 +0700

    Update ovpn.sh

--- ovpn.sh
+++ ovpn.sh
@@ -30,7 +30,7 @@ vps="aneka";
 #if [[ $vps = "zvur" ]]; then
 	#source="http://"
 #else
-	source="https://raw.githubusercontent.com/join-x/y"
+	source="https://raw.githubusercontent.com/yusuf-ardiansyah/y"
 #fi
 
 # go to root
